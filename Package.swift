d5b2a93f5986baf4acd4e057412f4f808348e01e
mwcs01 <jim@midwestcompsol.net>
2020-09-16 11:01:12 -0500

    Update Package.swift
    attempt five to fix SPM

--- Package.swift
+++ Package.swift
@@ -17,8 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "UICircularProgressRing",
-            dependencies: [],
-            path: "Sources"
+            dependencies: []
         )
     ]
 )
